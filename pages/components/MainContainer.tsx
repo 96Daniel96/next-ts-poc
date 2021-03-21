fed62090570605d31155def649b1d7421f1184d8
96Daniel96 <the.daniel.basilio@gmail.com>
2021-03-21 19:13:29 +0000

    Added MainContainer component

--- pages/components/MainContainer.tsx
+++ pages/components/MainContainer.tsx
@@ -1,40 +1,20 @@
-import React, { useState } from "react";
-import AnimatedDiv, {
-  AnimatedDivAnimations,
-} from "../../components/animated-div";
-import DContainer from "../../components/dcontainer";
+import React, { useState } from 'react';
+import AnimatedDiv, { AnimatedDivAnimations } from '../../components/animated-div';
+import styles from './MainContainer.module.scss';
 
 const MainContainer: React.FC<{}> = () => {
+  const t = data => {
+    const t2 = 2;
+  };
   return (
-    /* <div
-      className="container"
-      style={{
-        width: "65%",
-        backgroundColor: "rgba(0, 0, 0, 0.12)",
-        height: "800px",
-      }}
-    >
-      <AnimatedDiv animation={AnimatedDivAnimations.APPEAR}>
-        <div
-          style={{
-            height: "50vh",
-            marginTop: "20vh",
-            backgroundColor: "rgba(0, 0, 0, 0.25)",
-          }}
-        >
-          <div class="container">asd</div>
-        </div>
-      </AnimatedDiv>
-    </div> */
     <div
-      className="example-container"
+      className={styles['main-container']}
       style={{
-        backgroundColor: "rgba(0, 0, 0, 0.12)",
+        backgroundColor: 'rgba(0, 0, 0, 0.12)',
       }}
     >
-      <div className="example-row">
-        <div className="example-content-main">Main content</div>
-        <div className="example-content-secondary">Secondary content</div>
+      <div className={styles['main-row']}>
+        <div className={styles['main-col']}></div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,25 @@
-import Head from "next/head";
-import React, { useState } from "react";
+import Head from 'next/head';
+import React, { useState } from 'react';
 
-import DContainer from "../components/dcontainer";
-import AnimatedDiv, { AnimatedDivAnimations } from "../components/animated-div";
-import MainContainer from "./components/MainContainer";
-import styles from "./index.module.scss";
+import DContainer from '../components/dcontainer';
+import AnimatedDiv, { AnimatedDivAnimations } from '../components/animated-div';
+import MainContainer from './components/MainContainer';
+import styles from './index.module.scss';
 
 const Home: React.FC<{}> = () => {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin%20Sans"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin%20Sans" rel="stylesheet" />
       </Head>
-      <div className={styles["home"]}>
+      <div className={styles['home']}>
         <DContainer
           config={{
-            direction: "row",
-            align: { horizontal: "center" },
-            backgroundImage: "/background.jpg",
+            direction: 'row',
+            align: { horizontal: 'center' },
+            backgroundImage: '/background.jpg',
           }}
-          style={{ position: "static" }}
+          style={{ position: 'static' }}
         >
           <MainContainer />
         </DContainer>
