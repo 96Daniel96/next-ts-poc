6b241946c89b271fae937f4c33f114ca593e21bf
96Daniel96 <the.daniel.basilio@gmail.com>
2021-04-20 23:09:44 +0100

    Generalized Card component: conditional rendering on title and children injection

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -2,26 +2,22 @@ import clsx from 'clsx';
 import styles from './styles.module.scss';
 
 type CardProps = {
-  title: string;
+  title?: string;
+  maxWidth?: string;
 };
 
-const Card: React.FC<CardProps> = ({ title }) => {
+const Card: React.FC<CardProps> = ({ title, maxWidth, children }) => {
   return (
-    <div className={clsx('container', styles['card'])}>
-      <div className={'row'}>
-        <div className={clsx('col', styles['col-title'])}>
-          <span>{title}</span>
+    <div className={clsx('container', styles['card'])} style={{ maxWidth }}>
+      {title && (
+        <div className={'row'}>
+          <div className={clsx('col', styles['col-title'])}>
+            <span>{title}</span>
+          </div>
         </div>
-      </div>
+      )}
       <div className={'row'}>
-        <div className={clsx('col', styles['col-content'])}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-          leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-          with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-          publishing software like Aldus PageMaker including versions of Lorem Ipsum
-        </div>
+        <div className={clsx('col', styles['col-content'])}>{children}</div>
       </div>
     </div>
   );
