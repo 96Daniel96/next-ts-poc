2802b09817f9a16c5dd1c9c6053a9c22d799ff6e
96Daniel96 <the.daniel.basilio@gmail.com>
2021-04-20 23:10:49 +0100

    First Card with some styling

--- pages/components/MainContainer.tsx
+++ pages/components/MainContainer.tsx
@@ -11,7 +11,18 @@ const MainContainer: React.FC<{}> = () => {
         backgroundColor: 'rgba(0, 0, 0, 0.12)',
       }}
     >
-      <Card title="PROFILE INFO"></Card>
+      <Card maxWidth={'47rem'}>
+        <div className="container">
+          <div className="row flex-column-reverse flex-sm-row pt-3 pb-3">
+            <div className="col-sm-6 d-flex justify-content-center">
+              <div className="d-flex flex-column justify-content-center"></div>
+            </div>
+            <div className="col-sm-6 d-flex justify-content-center">
+              <img src={'/image-placeholder.png'} height={200} width={200} />
+            </div>
+          </div>
+        </div>
+      </Card>
       <Card title="INTRODUCTION"></Card>
       <Card title="EDUCATION"></Card>
       <Card title="SKILLS"></Card>
