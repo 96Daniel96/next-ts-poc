9026cf1f50600805973548d2017904c25604b681
96Daniel96 <the.daniel.basilio@gmail.com>
2021-04-26 23:12:34 +0100

    Extended Card component. Now having className prop

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -6,11 +6,12 @@ import styles from './styles.module.scss';
 type CardProps = {
   title?: string;
   maxWidth?: string;
+  className?: string;
 };
 
-const Card: React.FC<CardProps> = ({ title, maxWidth, children }) => {
+const Card: React.FC<CardProps> = ({ title, maxWidth, className, children }) => {
   return (
-    <div className={clsx('container', styles['card'])} style={{ maxWidth }}>
+    <div className={clsx('container', styles['card'], className)} style={{ maxWidth }}>
       {title && (
         <div className={'row'}>
           <div className={clsx('col', styles['col-title'])}>
