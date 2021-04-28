b49d877eccf0c4e4af2cef9918e5c339108b8a1a
Daniel Basilio <the.daniel.basilio@gmail.com>
2021-04-28 18:32:01 +0100

    Init progress bar on SkillsBar

--- pages/components/SkillsCard/index.tsx
+++ pages/components/SkillsCard/index.tsx
@@ -17,17 +17,39 @@ const SkillsCard: React.FC<{}> = () => {
       <div className="container">
         <div className={clsx('row', styles['skills-row'])}>
           <div className="col-md">
-            <div className="d-flex justify-content-center">
+            <div className="d-flex flex-column justify-content-center">
               <span>Title 1</span>
+              <div
+                className="skillbar"
+                style={{ backgroundColor: 'rgb(81, 59, 44)', transitionProperty: 'width, background-color' }}
+              >
+                {/*  <div className="skillbar-title">
+                  <span>HTML5</span>
+                </div> */}
+                <div
+                  className="skillbar-bar"
+                  style={{
+                    width: '61%',
+                    visibility: 'visible',
+                    transform: 'scale(1)',
+                    opacity: 0,
+                    transition:
+                      'transform 1.8s cubic-bezier(0.6, 0.2, 0.1, 1) 0.3s, opacity 1.8s cubic-bezier(0.6, 0.2, 0.1, 1) 0.3s',
+                    height: '4px',
+                    backgroundColor: 'white',
+                  }}
+                ></div>
+                {/* <div className="skill-bar-percent">90%</div> */}
+              </div>
             </div>
           </div>
           <div className="col-md">
-            <div className="d-flex justify-content-center">
+            <div className="d-flex flex-column justify-content-center">
               <span>Title 2</span>
             </div>
           </div>
           <div className="col-md">
-            <div className="d-flex justify-content-center">
+            <div className="d-flex flex-column justify-content-center">
               <span>Title 3</span>
             </div>
           </div>
