55c9e91ccc0692f5c51bd611b99f0b34b34f1651
96Daniel96 <the.daniel.basilio@gmail.com>
2021-04-29 21:26:05 +0100

    Inline styling for main paper on Timeline

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import MuiTimeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
@@ -7,24 +6,12 @@ import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { Box } from '@material-ui/core';
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    padding: '6px 16px',
-  },
-  secondaryTail: {
-    backgroundColor: theme.palette.secondary.main,
-  },
-}));
 
 type TimeLineItem = {
   title: string;
   secondary: string;
   timeline: string;
   description: string;
-  notes: string;
   dot?: JSX.Element;
 };
 
@@ -33,18 +20,16 @@ type TimelineProps = {
 };
 
 const Timeline: React.FC<TimelineProps> = ({ items }) => {
-  const classes = useStyles();
-
   return (
     <MuiTimeline align="alternate">
-      {items.map(({ title, secondary, timeline, description, notes, dot }, index) => (
+      {items.map(({ title, secondary, timeline, description, dot }, index) => (
         <TimelineItem key={index}>
           <TimelineSeparator>
             <TimelineDot>{dot}</TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            <Paper elevation={3} className={classes.paper}>
+            <Paper elevation={3} style={{ padding: '6px 16px' }}>
               <div className="d-flex flex-column">
                 <span style={{ fontSize: '1.3rem' }}>{title}</span>
                 {<span style={{ fontSize: '1.0rem' }}>{secondary}</span>}
@@ -52,7 +37,6 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
                   {timeline}
                 </span>
                 <span style={{ fontSize: '0.950rem', textAlign: 'justify' }}>{description}</span>
-                <span style={{ fontSize: '0.950rem', textAlign: 'justify' }}>{notes}</span>
               </div>
             </Paper>
           </TimelineContent>
